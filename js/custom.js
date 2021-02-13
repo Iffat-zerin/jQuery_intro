3963af899c3aec72188e5c673e2298a5cd639f51
Iffat-zerin <zeriniffat2@gmail.com>
2021-02-13 22:35:57 +0600

    first jQuery

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$('#btn1').click(function () {
+    $('.child').animate({
+        left: 225,
+        top: 225,
+        
+    }, 1000);
+$('.child').animate({
+        left: 450,
+        top:450,
+    
+    }, 1000);
+
+
+
+
+});
